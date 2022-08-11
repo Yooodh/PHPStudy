48696c9d6f66d78dd8fe0ebd50e0f376c9c7a2c5
Yooodh <eogus2604@hanmail.net>
2022-08-11 16:42:08 +0900

    v0.6

---
+++ LifeCoding/variable.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <h1>Variable</h1>
+  <?php
+  $a = 10;
+  echo $a+1;
+  ?>
+  <br />
+  <?php
+  $name = 'variable';
+  echo 'Lorem Ipsum is simply dummy text '.$name.' of the printing and typesetting industry. 
+  Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+  when an unknown printer took a '.$name.' galley of type and scrambled it to make a type specimen book. 
+  It has survived not only five centuries, 
+  but also the leap into electronic typesetting, remaining essentially unchanged. 
+  It was popularised in the 1960s '.$name.' with the release of Letraset sheets containing Lorem Ipsum passages, 
+  and more recently with desktop publishing '.$name.' software like Aldus PageMaker including versions of Lorem Ipsum.'
+  ?>
+</body>
+</html>

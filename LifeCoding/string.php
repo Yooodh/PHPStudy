53369ae83d25a06de97af6bce586ec99b592f85a
Yooodh <eogus2604@hanmail.net>
2022-08-11 16:28:19 +0900

    v0.5

---
+++ LifeCoding/string.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+  <body>
+    <h1>String & String Operator</h1>
+    <?php 
+      echo 'Hello word';
+    ?>
+    <br/>
+    <?php 
+      echo "Hello 'w'ord";
+    ?>
+    <br/>
+    <?php 
+      echo 'Hello "w"ord';
+    ?>
+    <h2>concatenation operator</h2>
+    <?php
+    echo "Hello "."world";
+    ?>
+    <h2>String length function</h2>
+    <?php
+    echo strlen ("Hello world");
+    ?>
+  </body>
+</html>

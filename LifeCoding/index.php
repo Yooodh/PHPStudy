8fa89dcdf10b13d3ca51e78ae71be714e1e7bc2a
Yooodh <eogus2604@hanmail.net>
2022-08-12 18:09:15 +0900

    v0.8

--- LifeCoding/index.php
+++ LifeCoding/index.php
@@ -1,8 +1,26 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
+  <head>
+    <meta charset='utf-8'>
+  </head>
   <body>
-    <?php
-    echo date('Y-m-d H:i:s');
-    ?>
+    <h1>WEB</h1>
+    <ol>
+      <li><a href='index.php?id=HTML'>HTML</a></li>
+      <li><a href='index.php?id=CSS'>CSS</a></li>
+      <li><a href='index.php?id=JavaScript'>JavaScript</a></li>
+    </ol>
+    <h2>
+      <?php
+        echo $_GET['id'];
+      ?>
+    </h2>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+      It has survived not only five centuries, 
+      but also the leap into electronic typesetting, remaining essentially unchanged. 
+      It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+      and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
   </body>
 </html>

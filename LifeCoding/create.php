24427f62ccc98557b5e904e18b73836e4552dba5
Yooodh <eogus2604@hanmail.net>
2022-08-23 02:59:10 +0900

    v2.2

--- LifeCoding/create.php
+++ LifeCoding/create.php
@@ -49,13 +49,16 @@
       ?>
     </ol>
       <a href="create.php">create</a>
-    <h2>
-      <?php
-        print_title();
-      ?>
-    </h2>
-    <?php
-      print_discription();
-    ?>
+      <form action="create_process.php" method='post'>
+        <p>
+          <input type="text" name='title' placeholder='Title'>
+        </p>
+        <p>
+          <textarea name="description" placeholder='Description'></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
   </body>
 </html>

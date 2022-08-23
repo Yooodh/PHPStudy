a79d46dc18fcbba3df461bb8e6ccaf3c039a828a
Yooodh <eogus2604@hanmail.net>
2022-08-24 01:23:31 +0900

    v2.5

--- LifeCoding/index.php
+++ LifeCoding/index.php
@@ -1,53 +1,8 @@
 <?php
-  function print_title() {
-    if(isset($_GET['id'])) {
-      echo $_GET['id'];
-    } else {
-      echo 'Welcome';
-    }
-  }
+  require_once('lib/print.php');
+  require_once('view/top.php');
+?>  
 
-  function print_discription() {
-    if(isset($_GET['id'])) {
-      echo file_get_contents('data/'.$_GET['id']);
-    } else {
-      echo 'Hello, PHP';
-    }
-  }
-
-  function print_list() {
-    $list = scandir('./data');
-    $i = 0;
-    while($i<count($list)){
-      if($list[$i] != '.') {
-        if($list[$i] != '..') {
-          ?>
-          <li><a href='index.php?id=<?=$list[$i]?>'><?=$list[$i]?></a></li>
-          <?php
-        }
-      }
-      $i = $i +1;
-    }
-  }
-?>
-
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset='utf-8'>
-    <title>
-    <?php
-      print_title();
-    ?>
-    </title>
-  </head>
-  <body>
-    <h1><a href='index.php'>WEB</a></h1>
-    <ol>
-      <?php
-        print_list();
-      ?>
-    </ol>
       <a href="create.php">create</a>
       <?php if(isset($_GET['id'])) { ?>
           <a href="update.php?id=<?=$_GET['id']?>">update</a>
@@ -62,7 +17,8 @@
       ?>
     </h2>
     <?php
-      print_discription();
+      print_description();
     ?>
-  </body>
-</html>
+<?php
+require('view/bottom.php');
+?>
